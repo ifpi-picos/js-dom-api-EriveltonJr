67296b90ba808e1834fee4920eff6c87e14916e4
EriveltonJr <franciscoerivelton12@gmail.com>
2024-04-08 22:27:47 -0300

    Correção de alguns erros na edição

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
-// Seleção de elementos
 const todoForm = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#todo-input");
 const todoList = document.querySelector("#todo-list");
 const editForm = document.querySelector("#edit-form");
 const editInput = document.querySelector("#edit-input");
+const editDateInput = document.querySelector("#edit-date-input");
 const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 const searchInput = document.querySelector("#search-input");
 const eraseBtn = document.querySelector("#erase-button");
@@ -12,13 +12,18 @@ const filterBtn = document.querySelector("#filter-select");
 let oldInputValue;
 
 // Funções
-const saveTodo = (text, done = 0, save = 1) => {
+
+const saveTodo = (text, date, done = 0, save = 1) => {
   const todo = document.createElement("div");
   todo.classList.add("todo");
 
   const todoTitle = document.createElement("h3");
   todoTitle.innerText = text;
   todo.appendChild(todoTitle);
+
+  const todoDate = document.createElement("p");
+  todoDate.innerText = "Data: " + date;
+  todo.appendChild(todoDate);
 
   const doneBtn = document.createElement("button");
   doneBtn.classList.add("finish-todo");
@@ -35,13 +40,12 @@ const saveTodo = (text, done = 0, save = 1) => {
   deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
   todo.appendChild(deleteBtn);
 
-  // Utilizando dados da localStorage
   if (done) {
     todo.classList.add("done");
   }
 
   if (save) {
-    saveTodoLocalStorage({ text, done: 0 });
+    saveTodoLocalStorage({ text, date, done: 0 });
   }
 
   todoList.appendChild(todo);
@@ -64,8 +68,22 @@ const updateTodo = (text) => {
     if (todoTitle.innerText === oldInputValue) {
       todoTitle.innerText = text;
 
-      // Utilizando dados da localStorage
       updateTodoLocalStorage(oldInputValue, text);
+    }
+  });
+};
+
+const updateTodoDate = (text, date) => {
+  const todos = document.querySelectorAll(".todo");
+
+  todos.forEach((todo) => {
+    let todoTitle = todo.querySelector("h3");
+
+    if (todoTitle.innerText === text) {
+      let todoDate = todo.querySelector("p");
+      todoDate.innerText = "Data: " + date;
+
+      updateTodoDateLocalStorage(text, date);
     }
   });
 };
@@ -92,40 +110,36 @@ const filterTodos = (filterValue) => {
   switch (filterValue) {
     case "all":
       todos.forEach((todo) => (todo.style.display = "flex"));
-
       break;
-
     case "done":
       todos.forEach((todo) =>
         todo.classList.contains("done")
           ? (todo.style.display = "flex")
           : (todo.style.display = "none")
       );
-
       break;
-
     case "todo":
       todos.forEach((todo) =>
         !todo.classList.contains("done")
           ? (todo.style.display = "flex")
           : (todo.style.display = "none")
       );
-
       break;
-
     default:
       break;
   }
 };
 
 // Eventos
+
 todoForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const inputValue = todoInput.value;
+  const inputDate = todoDate.value;
 
   if (inputValue) {
-    saveTodo(inputValue);
+    saveTodo(inputValue, inputDate);
   }
 });
 
@@ -147,7 +161,6 @@ document.addEventListener("click", (e) => {
   if (targetEl.classList.contains("remove-todo")) {
     parentEl.remove();
 
-    // Utilizando dados da localStorage
     removeTodoLocalStorage(todoTitle);
   }
 
@@ -156,6 +169,13 @@ document.addEventListener("click", (e) => {
 
     editInput.value = todoTitle;
     oldInputValue = todoTitle;
+
+    const todos = document.querySelectorAll(".todo");
+    todos.forEach((todo) => {
+      if (todo.querySelector("h3").innerText === todoTitle) {
+        editDateInput.value = todo.querySelector("p").innerText;
+      }
+    });
   }
 });
 
@@ -168,10 +188,13 @@ editForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const editInputValue = editInput.value;
+  const editDateValue = editDateInput.value;
 
   if (editInputValue) {
     updateTodo(editInputValue);
   }
+
+  updateTodoDate(editInputValue, editDateValue);
 
   toggleForms();
 });
@@ -207,7 +230,7 @@ const loadTodos = () => {
   const todos = getTodosLocalStorage();
 
   todos.forEach((todo) => {
-    saveTodo(todo.text, todo.done, 0);
+    saveTodo(todo.text, todo.date, todo.done, 0);
   });
 };
 
@@ -243,6 +266,18 @@ const updateTodoLocalStorage = (todoOldText, todoNewText) => {
   todos.map((todo) =>
     todo.text === todoOldText ? (todo.text = todoNewText) : null
   );
+
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
+
+const updateTodoDateLocalStorage = (todoText, newDate) => {
+  const todos = getTodosLocalStorage();
+
+  todos.forEach((todo) => {
+    if (todo.text === todoText) {
+      todo.date = newDate;
+    }
+  });
 
   localStorage.setItem("todos", JSON.stringify(todos));
 };
